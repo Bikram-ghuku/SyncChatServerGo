6b17d029625b17d4a555ae0ddfa0dc5abf10d07b
Bikram Ghuku <bikramghuku05@gmail.com>
2024-05-12 18:29:14 +0530

    feat: added users model

--- models/users.go
+++ models/users.go
@@ -1,1 +1,18 @@
 package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Users struct {
+	gorm.Model
+	UserId     uuid.UUID `gorm:"primaryKey type:uuid; default:uuid_generate_v4()" json:"userid"`
+	Password   string    `json:"password"`
+	Email      string    `gorm:"unique" json:"email"`
+	Name       string    `gorm:"unique" json:"name"`
+	Url        string    `gorm:"default:https://github.com/shadcn.png;" json:"profpic"`
+	LastOnline time.Time `json:"lastonline"`
+}
