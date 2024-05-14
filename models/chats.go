3b971954ee9e77cc0f33c7601df679efd6cc5934
Bikram Ghuku <bikramghuku05@gmail.com>
2024-05-14 11:09:40 +0530

    feat: updated chats model

--- models/chats.go
+++ models/chats.go
@@ -7,7 +7,8 @@ import (
 
 type Chats struct{
   gorm.Model
-  ChatId   uuid.UUID `gorm:"primaryKey type:uuid; deffault:uuid_generate_v4()" json:"userid"`
-  LastMsg  string    `json:"last_msg"`
-  SenderId string    `json:"sender_id"`
+  ChatId   uuid.UUID `gorm:"primaryKey type:uuid; deffault:uuid_generate_v4() column:chat_id" json:"userid"`
+  LastMsg  string    `gorm:"column:last_msg" json:"last_msg"`
+  SenderId string    `gorm:"column:sender_id" json:"sender_id"`
+  User     Users     `gorm:"column:user" json:"user" `
 }
