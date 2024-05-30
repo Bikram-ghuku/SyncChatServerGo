6007816e518b9fe4b426428777096379d47652c4
Bikram Ghuku <bikramghuku05@gmail.com>
2024-05-30 22:29:13 +0530

    Revert "Revert " Base Routing and Makefile ""

---
+++ controllers/channels.go
@@ -0,0 +1,24 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/Bikram-ghuku/SyncChatServerGo/models"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type userId struct {
+	User string `form:"user"`
+}
+
+func GetChannels(c *gin.Context, DB *gorm.DB) {
+	var senderData userId
+	err := c.BindJSON(&senderData)
+	if err != nil {
+		panic("Get channels error")
+	}
+
+	DB.First(&models.Users{}, senderData)
+	c.JSON(http.StatusOK, gin.H{"data": senderData.User})
+}
---
+++ routes/channels.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/Bikram-ghuku/SyncChatServerGo/controllers"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func SetupChannelsRoutes(DB *gorm.DB, superRoute *gin.RouterGroup) {
+	userRoutes := superRoute.Group("/channels")
+	{
+		userRoutes.GET("/", func(ctx *gin.Context) {
+			controllers.GetChannels(ctx, DB)
+		})
+	}
+}
