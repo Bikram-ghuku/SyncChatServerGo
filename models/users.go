60812b211057a18ce54580c1b1e33d181c7f21f9
Bikram Ghuku <bikramghuku05@gmail.com>
2024-05-30 22:28:13 +0530

    Revert " Base Routing and Makefile "

--- models/users.go
+++ models/users.go
@@ -7,10 +7,10 @@ import (
 )
 
 type Users struct {
-	UserId     uuid.UUID `gorm:"primaryKey; column:user_id; type:uuid; default:uuid_generate_v4()" json:"userid"`
+  UserId     uuid.UUID `gorm:"primaryKey; column:user_id; type:uuid; default:uuid_generate_v4()" json:"userid"`
 	Password   string    `gorm:"not null" json:"password"`
 	Email      string    `gorm:"unique; not null" json:"email"`
 	Name       string    `gorm:"unique; not null" json:"name"`
 	Url        string    `gorm:"default:https://github.com/shadcn.png;" json:"profpic"`
-	LastOnline time.Time `gorm:"default:CURRENT_TIMESTAMP; not null" json:"lastonline"`
+  LastOnline time.Time `gorm:"default:CURRENT_TIMESTAMP; not null" json:"lastonline"`
 }
