50731be81ab6be629f9e56e4d25102b0b575ea82
Bikram Ghuku <bikramghuku05@gmail.com>
2024-06-03 12:06:37 +0530

    fix: duplicate user signin

--- controllers/user.go
+++ controllers/user.go
@@ -10,25 +10,38 @@ import (
 	"gorm.io/gorm"
 )
 
-type signUp struct {
-	Email string `json:"email"`
-	Name  string `json:"name"`
-	Pswd  string `json:"pswd"`
-}
-
 func Register(c *gin.Context, DB *gorm.DB) {
+	type signUp struct {
+		Email string `json:"email"`
+		Name  string `json:"name"`
+		Pswd  string `json:"pswd"`
+	}
+
 	regUser := signUp{}
 
 	if err := c.BindJSON(&regUser); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"message": "error in request"})
-		panic("error in request")
+		return
+	}
+
+	findUser := models.Users{}
+	result := DB.Find(&findUser, "email = ?", regUser.Email)
+
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Database error"})
+		return
+	}
+
+	if result.RowsAffected != 0 {
+		c.JSON(http.StatusConflict, gin.H{"message": "User already exists"})
+		return
 	}
 
 	hashPswd, err := services.HashPassword(regUser.Pswd)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Error hashing password"})
-		panic("Error hashing password")
+		return
 	}
 
 	var newUser = models.Users{
@@ -42,7 +55,7 @@ func Register(c *gin.Context, DB *gorm.DB) {
 
 	if resData.Error != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Error creating new user"})
-		panic("Error creating new user")
+		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "User created successfully"})
