9a3e3be4cfe7137a2b25ed695b0f557e475b3293
Bikram Ghuku <bikramghuku05@gmail.com>
2024-06-04 14:20:47 +0530

    feat: added jwt middle data receiver

--- controllers/channels.go
+++ controllers/channels.go
@@ -1,26 +1,41 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
-type userId struct {
-	User string `json:"user"`
+type ClaimStruct struct {
+	Email  string    `json:"email"`
+	Exp    int64     `json:"exp"`
+	UserId uuid.UUID `json:"userId"`
 }
 
 func GetChannels(c *gin.Context, DB *gorm.DB) {
-	senderData := userId{}
-
-	if err := c.BindJSON(&senderData); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "username and password is needed"})
-		panic("Get channels error")
-	}
-	c.Bind(&senderData)
-	fmt.Println(senderData.User)
 	//DB.First(&models.Users{}, senderData)
-	c.JSON(http.StatusOK, gin.H{"data": senderData.User})
+	data, present := c.Get("data")
+	if !present {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "JWT authorisation failed"})
+	}
+	data_json, err := json.Marshal(data)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "JWT data error"})
+	}
+
+	claimStruct := ClaimStruct{}
+
+	err = json.Unmarshal(data_json, &claimStruct)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(claimStruct.Email)
+	fmt.Println(string(data_json))
+	c.JSON(http.StatusOK, gin.H{"data": data})
 }
