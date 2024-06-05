cca76ddb8b6fa7029a8d7e682cb89860bb2764a3
Bikram Ghuku <bikramghuku05@gmail.com>
2024-06-05 11:58:58 +0530

    feat: added addChannels Route

--- backend/controllers/channels.go
+++ backend/controllers/channels.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/Bikram-ghuku/SyncChatServerGo/models"
@@ -17,8 +16,7 @@ type ClaimStruct struct {
 	UserId uuid.UUID `json:"userId"`
 }
 
-func GetChannels(c *gin.Context, DB *gorm.DB) {
-	//DB.First(&models.Users{}, senderData)
+func getJwtData(c *gin.Context) ClaimStruct {
 	data, present := c.Get("data")
 	if !present {
 		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "JWT authorisation failed"})
@@ -36,6 +34,14 @@ func GetChannels(c *gin.Context, DB *gorm.DB) {
 		panic(err)
 	}
 
+	return claimStruct
+}
+
+func GetChannels(c *gin.Context, DB *gorm.DB) {
+	//DB.First(&models.Users{}, senderData)
+
+	claimStruct := getJwtData(c)
+
 	findChannels := models.Chats{}
 	result := DB.Find(&findChannels, "sender_id = ?", claimStruct.UserId)
 
@@ -43,11 +49,70 @@ func GetChannels(c *gin.Context, DB *gorm.DB) {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "Database error"})
 		return
 	}
-	fmt.Println(result.RowsAffected)
+
 	if result.RowsAffected == 0 {
 		c.JSON(http.StatusOK, gin.H{"data": gin.H{}})
 	} else {
 		c.JSON(http.StatusOK, gin.H{"data": findChannels})
 
 	}
+}
+
+func AddChannels(c *gin.Context, DB *gorm.DB) {
+	type ReqData struct {
+		Email string `json:"email"`
+	}
+	claimsStruct := getJwtData(c)
+
+	reqData := ReqData{}
+
+	if err := c.BindJSON(&reqData); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "error in request"})
+		return
+	}
+
+	findUser := models.Users{}
+	result := DB.Find(&findUser, "email = ?", reqData.Email)
+
+	if result.Error != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "Database error"})
+		return
+	}
+
+	if result.RowsAffected == 0 {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "No user with the given Email found"})
+		return
+	}
+
+	result = DB.Raw("SELECT DISTINCT a.chat_id, a.sender_id, b.sender_id FROM chats_data a INNER JOIN chats_data b ON a.chat_id = b.chat_id AND a.sender_id != b.sender_id WHERE a.sender_id = ? AND b.sender_id = ? ", claimsStruct.UserId, findUser.UserId)
+
+	if result.RowsAffected != 0 {
+		c.AbortWithStatusJSON(http.StatusConflict, gin.H{"message": "Channel between the users exsists"})
+		return
+	}
+
+	newChat_a := models.Chats{
+		SenderId: claimsStruct.UserId,
+	}
+
+	result = DB.Create(&newChat_a)
+
+	if result.Error != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "Error creating new Chat"})
+		return
+	}
+
+	newChat_b := models.Chats{
+		SenderId: findUser.UserId,
+		ChatId:   newChat_a.ChatId,
+	}
+
+	result = DB.Create(&newChat_b)
+
+	if result.Error != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "Error creating new Chat"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Chat successfully created"})
 }
