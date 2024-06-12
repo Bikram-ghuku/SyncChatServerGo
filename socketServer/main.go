fee2a0ecc715bd1c94ff49846830ea0462cdcc8c
Bikram Ghuku <bikramghuku05@gmail.com>
2024-06-12 13:04:37 +0530

    feat: added inital socket server

--- socketServer/main.go
+++ socketServer/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	socketio "github.com/googollee/go-socket.io"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	server := socketio.NewServer(nil)
+
+	port := os.Getenv("PORT")
+
+	go server.Serve()
+	defer server.Close()
+
+	http.Handle("/", server)
+	log.Printf("Serving at localhost:%s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", port), nil))
+}
