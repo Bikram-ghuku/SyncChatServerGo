7f2b5c0faa0aff99fa1aa908dd8323fbd3974262
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-01 12:03:39 +0530

    feat: added redis modules

--- socketServer/main.go
+++ socketServer/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/go-redis/redis/v8"
 	socketio "github.com/googollee/go-socket.io"
 	"github.com/joho/godotenv"
 )
@@ -14,20 +16,29 @@ var (
 	server *socketio.Server
 )
 
+var redisClient = redis.NewClient(&redis.Options{
+	Addr: "localhost:6379",
+})
+
+var ctx = context.Background()
+
 func Handler() {
 	server.OnConnect("/", func(c socketio.Conn) error {
+		c.SetContext("")
 		fmt.Println("A new User conencted: ", c.ID())
 		return nil
 	})
 	server.OnEvent("/", "message", func(s socketio.Conn, msg string) {
-
+		if err := redisClient.Publish(ctx, "message", msg).Err(); err != nil {
+			fmt.Println("Error publishing to redis: ", err.Error())
+		}
 	})
 }
 
 func main() {
 	godotenv.Load()
 	server = socketio.NewServer(nil)
-
+	Handler()
 	port := os.Getenv("PORT")
 
 	go server.Serve()
