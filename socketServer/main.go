03bf688c26410c84301388e3f3fd765f29d45e80
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-04 19:54:17 +0530

    feat: added redis

--- socketServer/main.go
+++ socketServer/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/go-redis/redis"
 	"github.com/gorilla/websocket"
 	"github.com/joho/godotenv"
 )
@@ -17,6 +18,10 @@ var upgrader = websocket.Upgrader{
 		return true
 	},
 }
+
+var redisClient = redis.NewClient(&redis.Options{
+	Addr: "localhost:6379",
+})
 
 func Handler(res http.ResponseWriter, req *http.Request) {
 	c, err := upgrader.Upgrade(res, req, nil)
@@ -31,7 +36,10 @@ func Handler(res http.ResponseWriter, req *http.Request) {
 			log.Println(err.Error())
 			break
 		}
-		log.Printf("Received: %s", msg)
+		//log.Printf("Received: %s", msg)
+		if err := redisClient.Publish("send-user-data", msg).Err(); err != nil {
+			panic(err)
+		}
 		err = c.WriteMessage(mt, msg)
 		if err != nil {
 			log.Println("write: ", err)
@@ -42,8 +50,14 @@ func Handler(res http.ResponseWriter, req *http.Request) {
 
 func main() {
 	godotenv.Load()
+	if err := redisClient.Ping().Err(); err != nil {
+		panic(err.Error())
+	}
+
 	http.HandleFunc("/", Handler)
 	port := os.Getenv("PORT")
 	log.Printf("Serving at localhost:%s", port)
+
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", port), nil))
+
 }
