a9828b7e7751bcbd7a5dfb8e64fbc48f84735cdd
Bikram Ghuku <bikramghuku05@gmail.com>
2025-10-05 20:32:14 +0530

    feat: message storage

--- socket_server/services/KafkaClient.ts
+++ socket_server/services/KafkaClient.ts
@@ -1,47 +1,56 @@
-import { Consumer, Kafka, Producer } from "kafkajs";
+import { Consumer, Kafka, Producer } from 'kafkajs'
+import DB from './PostgresDB'
 
 const kafka = new Kafka({
-    clientId: 'socketInjest',
-    brokers: [process.env.KAFKA_BROKERS || "localhost:9092"],
-});
+	clientId: 'socketInjest',
+	brokers: [process.env.KAFKA_BROKERS || 'localhost:9092'],
+})
 
-export const kafkaTopic = process.env.KAFKA_TOPIC || "sync-chat-msg";
+export const kafkaTopic = process.env.KAFKA_TOPIC || 'sync-chat-msg'
 
 export async function initKafkaProducer() {
-    const producer: Producer = kafka.producer({
-        allowAutoTopicCreation: true,
-    });
+	const producer: Producer = kafka.producer({
+		allowAutoTopicCreation: true,
+	})
 
-    try {
-        await producer.connect();
-        return producer;
-    } catch (error) {
-        console.error("Failed to connect Kafka producer:", error);
-        throw error;
-    }
+	try {
+		await producer.connect()
+		return producer
+	} catch (error) {
+		console.error('Failed to connect Kafka producer:', error)
+		throw error
+	}
 }
 
 export async function initKafkaConsumer() {
-    const consumer: Consumer = kafka.consumer({ groupId: 'socketInjest-group', allowAutoTopicCreation: true });
-    try {
-        await consumer.connect();
-        await consumer.subscribe({ topic: kafkaTopic, fromBeginning: false });
-        consumer.on('consumer.crash', (e) => {
-            console.error("Kafka consumer crashed:", e);
-            process.exit(1);
-        });
-        await consumer.run({
-            eachMessage: async ({ topic, partition, message }) => {
-                console.log({
-                    value: message.value?.toString(),
-                    topic,
-                    partition,
-                });
-            },
-        });
-    } catch (error) {
-        console.error("Failed to connect Kafka consumer:", error);
-        throw error;
-    }
-
+	const consumer: Consumer = kafka.consumer({
+		groupId: 'socketInjest-group',
+		allowAutoTopicCreation: true,
+	})
+	try {
+		await consumer.connect()
+		await consumer.subscribe({ topic: kafkaTopic, fromBeginning: false })
+		consumer.on('consumer.crash', e => {
+			console.error('Kafka consumer crashed:', e)
+			process.exit(1)
+		})
+		await consumer.run({
+			eachMessage: async ({ topic, partition, message }) => {
+				const { jwt, msg, chatId, timestamp } = JSON.parse(
+					message.value?.toString() || '{}'
+				)
+				DB.InsertIntoDB(msg, chatId, jwt, timestamp).catch(err => {
+					console.error('Failed to insert message into DB:', err)
+				})
+				console.log({
+					value: message.value?.toString(),
+					topic,
+					partition,
+				})
+			},
+		})
+	} catch (error) {
+		console.error('Failed to connect Kafka consumer:', error)
+		throw error
+	}
 }
--- socket_server/services/PostgresDB.ts
+++ socket_server/services/PostgresDB.ts
@@ -1,19 +1,34 @@
-import { Pool } from 'pg';
+import { Pool } from 'pg'
 
 const pool = new Pool({
-    user:
-        process.env.PGUSER || 'postgres',
-    host:
-        process.env.PGHOST || 'localhost',
-    database:
-        process.env.PGDATABASE || 'postgres',
-    password:
-        process.env.PGPASSWORD || 'password',
-    port:
-        parseInt(process.env.PGPORT || '5432', 10),
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-});
+	user: process.env.POSTGRES_USER || 'postgres',
+	host: process.env.PGHOST || 'localhost',
+	database: process.env.POSTGRES_DBNAME || 'postgres',
+	password: process.env.POSTGRES_PASSWORD || 'password',
+	port: parseInt(process.env.PGPORT || '5432', 10),
+	max: 20,
+	idleTimeoutMillis: 30000,
+	connectionTimeoutMillis: 2000,
+})
 
-export default pool;
+function parseJwt(token: string) {
+	return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString())
+}
+
+function InsertIntoDB(
+	message: string,
+	chatId: string,
+	token: string,
+	createdAt: string
+) {
+	const { userId } = parseJwt(token)
+	const query = `
+		INSERT INTO messages (msg, chat_id, user_id, created_at)
+		VALUES ($1, $2, $3, to_timestamp($4))
+	`
+	return pool.query(query, [message, chatId, userId, createdAt])
+}
+
+export default {
+	InsertIntoDB,
+}
